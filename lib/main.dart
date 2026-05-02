2d78f27427d36d3283590fa532116e242027d52f
Eric Hoang <h_eric56@yahoo.com>
2026-05-01 20:09:58 -0400

    feat: add firebase init and provider wiring to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'theme/app_theme.dart';
-import 'screens/auth/login_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'firebase_options.dart';
+import 'providers/user_provider.dart';
+import 'repositories/user_repository.dart';
+import 'screens/auth/login_screen.dart';
+import 'services/auth_service.dart';
+import 'services/firestore_service.dart';
+import 'theme/app_theme.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final authService = AuthService();
+  final firestoreService = FirestoreService();
+  final userRepository = UserRepository(
+    authService: authService,
+    firestoreService: firestoreService,
+  );
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(userRepository: userRepository),
+        ),
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
